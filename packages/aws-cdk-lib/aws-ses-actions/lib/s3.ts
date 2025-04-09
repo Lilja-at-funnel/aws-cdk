Lilja-at-funnel/aws-cdk

commit 2da544feeeda68a379f0f79f18e138b9640c1691

fix(ses-actions): permissions too wide for S3 action (#29833)
### Issue # (if applicable)

Closes #29811, continuation of @msambol 's #29823

### Reason for this change

Reduce overly broad permissions allocated to SES for the S3 receipt rule action

### Description of changes

* Restrain by both rule set and rule name, as recommended in the [docs](https://docs.aws.amazon.com/ses/latest/dg/receiving-email-permissions.html#receiving-email-permissions-s3)
	* Accomplished by generating the permission lazily, when the rule is rendering the actions for CloudFormation  

### Description of how you validated changes

Updated the unit and integration tests. The integration now uses a free test WorkMail domain. It's a bit of manual setup upfront, but doesn't require the contributor to use one of their own domains

### Checklist
- [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)

----

*By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

packages/aws-cdk-lib/aws-ses-actions/lib/s3.ts
@@ -42,31 +42,12 @@ export interface S3Props {
  * a notification to Amazon SNS.
  */
 export class S3 implements ses.IReceiptRuleAction {
+  private rule?: ses.IReceiptRule;
   constructor(private readonly props: S3Props) {
   }
 
   public bind(rule: ses.IReceiptRule): ses.ReceiptRuleActionConfig {
-    // Allow SES to write to S3 bucket
-    // See https://docs.aws.amazon.com/ses/latest/DeveloperGuide/receiving-email-permissions.html#receiving-email-permissions-s3
-    const keyPattern = this.props.objectKeyPrefix || '';
-    const s3Statement = new iam.PolicyStatement({
-      actions: ['s3:PutObject'],
-      principals: [new iam.ServicePrincipal('ses.amazonaws.com')],
-      resources: [this.props.bucket.arnForObjects(`${keyPattern}*`)],
-      conditions: {
-        StringEquals: {
-          'aws:Referer': cdk.Aws.ACCOUNT_ID,
-        },
-      },
-    });
-    this.props.bucket.addToResourcePolicy(s3Statement);
-
-    const policy = this.props.bucket.node.tryFindChild('Policy') as s3.BucketPolicy;
-    if (policy) { // The bucket could be imported
-      rule.node.addDependency(policy);
-    } else {
-      cdk.Annotations.of(rule).addWarningV2('@aws-cdk/s3:AddBucketPermissions', 'This rule is using a S3 action with an imported bucket. Ensure permission is given to SES to write to that bucket.');
-    }
+    this.rule = rule;
 
     // Allow SES to use KMS master key
     // See https://docs.aws.amazon.com/ses/latest/DeveloperGuide/receiving-email-permissions.html#receiving-email-permissions-kms
@@ -97,5 +78,42 @@ export class S3 implements ses.IReceiptRuleAction {
         topicArn: this.props.topic?.topicArn,
       },
     };
+  }
+
+  /**
+   * Generate and apply the receipt rule action statement
+   *
+   * @param ruleSet The rule set the rule is being added to
+   * @internal
+   */
+  public _applyPolicyStatement(receiptRuleSet: ses.IReceiptRuleSet): void {
+    if (!this.rule) {
+      throw new Error('Cannot apply policy statement before binding the action to a receipt rule');
+    }
+
+    // Allow SES to write to S3 bucket
+    // See https://docs.aws.amazon.com/ses/latest/DeveloperGuide/receiving-email-permissions.html#receiving-email-permissions-s3
+    const keyPattern = this.props.objectKeyPrefix || '';
+    const s3Statement = new iam.PolicyStatement({
+      actions: ['s3:PutObject'],
+      principals: [new iam.ServicePrincipal('ses.amazonaws.com')],
+      resources: [this.props.bucket.arnForObjects(`${keyPattern}*`)],
+      conditions: {
+        StringEquals: {
+          'aws:SourceAccount': cdk.Aws.ACCOUNT_ID,
+          'aws:SourceArn': cdk.Arn.format({
+            partition: cdk.Aws.PARTITION,
+            service: 'ses',
+            region: cdk.Aws.REGION,
+            account: cdk.Aws.ACCOUNT_ID,
+            resource: [
+              `receipt-rule-set/${receiptRuleSet.receiptRuleSetName}`,
+              `receipt-rule/${this.rule.receiptRuleName}`,
+            ].join(':'),
+          }),
+        },
+      },
+    });
+    this.props.bucket.addToResourcePolicy(s3Statement);
   }
 }

packages/aws-cdk-lib/aws-ses-actions/test/actions.test.ts
@@ -178,9 +178,6 @@ test('add s3 action', () => {
         Ref: 'RuleSetE30C6C48',
       },
     },
-    DependsOn: [
-      'BucketPolicyE9A3008A',
-    ],
   });
 
   Template.fromStack(stack).hasResourceProperties('AWS::S3::BucketPolicy', {
@@ -193,8 +190,25 @@ test('add s3 action', () => {
           Action: 's3:PutObject',
           Condition: {
             StringEquals: {
-              'aws:Referer': {
+              'aws:SourceAccount': {
                 Ref: 'AWS::AccountId',
+              },
+              'aws:SourceArn': {
+                'Fn::Join': [
+                  '',
+                  [
+                    'arn:',
+                    { Ref: 'AWS::Partition' },
+                    ':ses:',
+                    { Ref: 'AWS::Region' },
+                    ':',
+                    { Ref: 'AWS::AccountId' },
+                    ':receipt-rule-set/',
+                    { Ref: 'RuleSetE30C6C48' },
+                    ':receipt-rule/',
+                    { Ref: 'RuleSetRule0B1D6BCA' },
+                  ],
+                ],
               },
             },
           },

packages/@aws-cdk-testing/framework-integ/test/aws-ses-actions/test/integ.actions.js.snapshot/asset.96d0b6be9a64ae309bf89a86f5515453f0fa1d07b4f6b37198051cc98e251f34/index.d.ts
@@ -1,3 +1,0 @@
-export declare function handler(event: AWSLambda.SESEvent): Promise<{
-    disposition: string;
-} | null>;

packages/@aws-cdk-testing/framework-integ/test/aws-ses-actions/test/integ.actions.js.snapshot/asset.96d0b6be9a64ae309bf89a86f5515453f0fa1d07b4f6b37198051cc98e251f34/index.js
@@ -1,22 +1,0 @@
-"use strict";
-/* eslint-disable no-console */
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handler = void 0;
-// Adapted from https://docs.aws.amazon.com/ses/latest/DeveloperGuide/receiving-email-action-lambda-example-functions.html
-async function handler(event) {
-    console.log('Spam filter');
-    const sesNotification = event.Records[0].ses;
-    console.log('SES Notification: %j', sesNotification);
-    // Check if any spam check failed
-    if (sesNotification.receipt.spfVerdict.status === 'FAIL'
-        || sesNotification.receipt.dkimVerdict.status === 'FAIL'
-        || sesNotification.receipt.spamVerdict.status === 'FAIL'
-        || sesNotification.receipt.virusVerdict.status === 'FAIL') {
-        console.log('Dropping spam');
-        // Stop processing rule set, dropping message
-        return { disposition: 'STOP_RULE_SET' };
-    }
-    return null;
-}
-exports.handler = handler;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUEsK0JBQStCOzs7QUFFL0IsMEhBQTBIO0FBQ25ILEtBQUssVUFBVSxPQUFPLENBQUMsS0FBeUI7SUFDckQsT0FBTyxDQUFDLEdBQUcsQ0FBQyxhQUFhLENBQUMsQ0FBQztJQUUzQixNQUFNLGVBQWUsR0FBRyxLQUFLLENBQUMsT0FBTyxDQUFDLENBQUMsQ0FBQyxDQUFDLEdBQUcsQ0FBQztJQUM3QyxPQUFPLENBQUMsR0FBRyxDQUFDLHNCQUFzQixFQUFFLGVBQWUsQ0FBQyxDQUFDO0lBRXJELGlDQUFpQztJQUNqQyxJQUFJLGVBQWUsQ0FBQyxPQUFPLENBQUMsVUFBVSxDQUFDLE1BQU0sS0FBSyxNQUFNO1dBQ2pELGVBQWUsQ0FBQyxPQUFPLENBQUMsV0FBVyxDQUFDLE1BQU0sS0FBSyxNQUFNO1dBQ3JELGVBQWUsQ0FBQyxPQUFPLENBQUMsV0FBVyxDQUFDLE1BQU0sS0FBSyxNQUFNO1dBQ3JELGVBQWUsQ0FBQyxPQUFPLENBQUMsWUFBWSxDQUFDLE1BQU0sS0FBSyxNQUFNLEVBQUU7UUFDN0QsT0FBTyxDQUFDLEdBQUcsQ0FBQyxlQUFlLENBQUMsQ0FBQztRQUU3Qiw2Q0FBNkM7UUFDN0MsT0FBTyxFQUFFLFdBQVcsRUFBRSxlQUFlLEVBQUUsQ0FBQztLQUN6QztJQUVELE9BQU8sSUFBSSxDQUFDO0FBQ2QsQ0FBQztBQWxCRCwwQkFrQkMiLCJzb3VyY2VzQ29udGVudCI6WyIvKiBlc2xpbnQtZGlzYWJsZSBuby1jb25zb2xlICovXG5cbi8vIEFkYXB0ZWQgZnJvbSBodHRwczovL2RvY3MuYXdzLmFtYXpvbi5jb20vc2VzL2xhdGVzdC9EZXZlbG9wZXJHdWlkZS9yZWNlaXZpbmctZW1haWwtYWN0aW9uLWxhbWJkYS1leGFtcGxlLWZ1bmN0aW9ucy5odG1sXG5leHBvcnQgYXN5bmMgZnVuY3Rpb24gaGFuZGxlcihldmVudDogQVdTTGFtYmRhLlNFU0V2ZW50KTogUHJvbWlzZTx7IGRpc3Bvc2l0aW9uOiBzdHJpbmcgfSB8IG51bGw+IHtcbiAgY29uc29sZS5sb2coJ1NwYW0gZmlsdGVyJyk7XG5cbiAgY29uc3Qgc2VzTm90aWZpY2F0aW9uID0gZXZlbnQuUmVjb3Jkc1swXS5zZXM7XG4gIGNvbnNvbGUubG9nKCdTRVMgTm90aWZpY2F0aW9uOiAlaicsIHNlc05vdGlmaWNhdGlvbik7XG5cbiAgLy8gQ2hlY2sgaWYgYW55IHNwYW0gY2hlY2sgZmFpbGVkXG4gIGlmIChzZXNOb3RpZmljYXRpb24ucmVjZWlwdC5zcGZWZXJkaWN0LnN0YXR1cyA9PT0gJ0ZBSUwnXG4gICAgICB8fCBzZXNOb3RpZmljYXRpb24ucmVjZWlwdC5ka2ltVmVyZGljdC5zdGF0dXMgPT09ICdGQUlMJ1xuICAgICAgfHwgc2VzTm90aWZpY2F0aW9uLnJlY2VpcHQuc3BhbVZlcmRpY3Quc3RhdHVzID09PSAnRkFJTCdcbiAgICAgIHx8IHNlc05vdGlmaWNhdGlvbi5yZWNlaXB0LnZpcnVzVmVyZGljdC5zdGF0dXMgPT09ICdGQUlMJykge1xuICAgIGNvbnNvbGUubG9nKCdEcm9wcGluZyBzcGFtJyk7XG5cbiAgICAvLyBTdG9wIHByb2Nlc3NpbmcgcnVsZSBzZXQsIGRyb3BwaW5nIG1lc3NhZ2VcbiAgICByZXR1cm4geyBkaXNwb3NpdGlvbjogJ1NUT1BfUlVMRV9TRVQnIH07XG4gIH1cblxuICByZXR1cm4gbnVsbDtcbn1cbiJdfQ==

packages/@aws-cdk-testing/framework-integ/test/aws-ses-actions/test/integ.actions.js.snapshot/asset.96d0b6be9a64ae309bf89a86f5515453f0fa1d07b4f6b37198051cc98e251f34/index.ts
@@ -1,22 +1,0 @@
-/* eslint-disable no-console */
-
-// Adapted from https://docs.aws.amazon.com/ses/latest/DeveloperGuide/receiving-email-action-lambda-example-functions.html
-export async function handler(event: AWSLambda.SESEvent): Promise<{ disposition: string } | null> {
-  console.log('Spam filter');
-
-  const sesNotification = event.Records[0].ses;
-  console.log('SES Notification: %j', sesNotification);
-
-  // Check if any spam check failed
-  if (sesNotification.receipt.spfVerdict.status === 'FAIL'
-      || sesNotification.receipt.dkimVerdict.status === 'FAIL'
-      || sesNotification.receipt.spamVerdict.status === 'FAIL'
-      || sesNotification.receipt.virusVerdict.status === 'FAIL') {
-    console.log('Dropping spam');
-
-    // Stop processing rule set, dropping message
-    return { disposition: 'STOP_RULE_SET' };
-  }
-
-  return null;
-}

packages/@aws-cdk-testing/framework-integ/test/aws-ses-actions/test/integ.actions.js.snapshot/cdk.out
@@ -1,1 +1,1 @@
-{"version":"34.0.0"}
+{"version":"36.0.0"}

packages/@aws-cdk-testing/framework-integ/test/aws-ses-actions/test/integ.actions.ts
@@ -4,18 +4,23 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as ses from 'aws-cdk-lib/aws-ses';
 import * as sns from 'aws-cdk-lib/aws-sns';
 import * as cdk from 'aws-cdk-lib';
+import * as subscriptions from 'aws-cdk-lib/aws-sns-subscriptions';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as actions from 'aws-cdk-lib/aws-ses-actions';
 import { STANDARD_NODEJS_RUNTIME } from '../../config';
 
-/**********************************************************************************************************************
- *
- *    Warning! This test case can not be deployed!
- *
- *    Save yourself some time and move on.
- *    The latest given reason is:
- *    - 2023-08-30: Uses a hardcoded email address that is not verified, @mrgrain
- *
- *********************************************************************************************************************/
+/**
+ * 1. Create a free Workmail test domain (https://us-east-1.console.aws.amazon.com/workmail/v2/home?region=us-east-1#/organizations/create)
+ *  - It should automatically be added to your list of verified SES domains, no need to exit the SES sandbox
+ * 2. Add a new user email address in the Workmail console
+ * 3. Update the TEST_EMAIL constant with the email address of the user you created
+ * 4. Deploy the stack with --no-clean, and send an email to the email address you created
+ * 5. Check the following:
+ *  - The email should be saved to the S3 bucket
+ *  - The SQS queue should receive receipt notifications
+ */
+
+const TEST_EMAIL = 'test@cdk-test-123.awsapps.com';
 
 const app = new cdk.App();
 
@@ -33,9 +38,12 @@ const bucket = new s3.Bucket(stack, 'Bucket');
 
 const kmsKey = new kms.Key(stack, 'Key');
 
-const ruleSet = new ses.ReceiptRuleSet(stack, 'RuleSet', {
-  dropSpam: true,
-});
+const ruleSet = ses.ReceiptRuleSet.fromReceiptRuleSetName(
+  stack,
+  'RuleSet',
+  // Default WorkMail rule set
+  'INBOUND_MAIL',
+);
 
 const firstRule = ruleSet.addRule('FirstRule', {
   actions: [
@@ -60,13 +68,13 @@ const firstRule = ruleSet.addRule('FirstRule', {
     }),
   ],
   receiptRuleName: 'FirstRule',
-  recipients: ['cdk-ses-receipt-test@yopmail.com'],
+  recipients: [TEST_EMAIL],
   scanEnabled: true,
   tlsPolicy: ses.TlsPolicy.REQUIRE,
 });
 
 firstRule.addAction(new actions.Bounce({
-  sender: 'cdk-ses-receipt-test@yopmail.com',
+  sender: TEST_EMAIL,
   template: actions.BounceTemplate.MESSAGE_CONTENT_REJECTED,
   topic,
 }));
@@ -76,5 +84,8 @@ const secondRule = ruleSet.addRule('SecondRule');
 secondRule.addAction(new actions.Stop({
   topic,
 }));
+
+const queue = new sqs.Queue(stack, 'NotificationQueue');
+topic.addSubscription(new subscriptions.SqsSubscription(queue));
 
 app.synth();

packages/aws-cdk-lib/aws-ses/lib/receipt-rule-action.ts
@@ -1,4 +1,5 @@
 import { IReceiptRule } from './receipt-rule';
+import { IReceiptRuleSet } from './receipt-rule-set';
 
 /**
  * An abstract action for a receipt rule.
@@ -8,6 +9,14 @@ export interface IReceiptRuleAction {
    * Returns the receipt rule action specification
    */
   bind(receiptRule: IReceiptRule): ReceiptRuleActionConfig;
+
+  /**
+   * Generate and apply the receipt rule action statement
+   *
+   * @param ruleSet The rule set the rule is being added to
+   * @internal
+   */
+  _applyPolicyStatement?(ruleSet: IReceiptRuleSet): void;
 }
 
 /**

commit e7aa05280964788fd4eac5a7a8b34447fd54357d

fix(aws-ses-actions): Condition should use SourceAccount
Restricts the SES to come through the SourceAccount in question.

This will change does not affect bucket policy and ses rule action race
condition, reported in #30143 and introduced in #29833 and reverted in #30375.
That PR introduced the rule set name into the bucket policy, which added
a dependency to the policy to the rule set(while the rule set requires
that the policy is created first). Doing this change made a circular
dependency between the two resources.

This change solves #29811

packages/aws-cdk-lib/aws-ses-actions/lib/s3.ts
@@ -55,7 +55,7 @@ export class S3 implements ses.IReceiptRuleAction {
       resources: [this.props.bucket.arnForObjects(`${keyPattern}*`)],
       conditions: {
         StringEquals: {
-          'aws:Referer': cdk.Aws.ACCOUNT_ID,
+          'aws:SourceAccount': cdk.Aws.ACCOUNT_ID,
         },
       },
     });

packages/aws-cdk-lib/aws-ses-actions/test/actions.test.ts
@@ -190,7 +190,7 @@ test('add s3 action', () => {
           Action: 's3:PutObject',
           Condition: {
             StringEquals: {
-              'aws:Referer': {
+              'aws:SourceAccount': {
                 Ref: 'AWS::AccountId',
               },
             },